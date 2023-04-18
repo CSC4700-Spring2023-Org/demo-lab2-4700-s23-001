481d83c6bfac60d0019a3e96bc955b08b591ee2e
ms-O <kristin.obermyer@villanova.edu>
2023-04-18 16:21:28 -0400

    In-class demo of initial tests

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,9 +1,16 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CurrencyConverter {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Another statement");
+    }
 
+    public BigDecimal convert(BigDecimal originalAmount, BigDecimal conversionRate) {
+        BigDecimal result = originalAmount.multiply(conversionRate);
+        return result.setScale(2, RoundingMode.HALF_UP);
     }
 
 }
